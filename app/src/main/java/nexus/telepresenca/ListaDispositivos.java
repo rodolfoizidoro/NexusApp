a2121c60c555a62a2a16865997ae9bd9bed5f7cf
rodolfoizidoro <kethupe@hotmail.com>
2016-08-30 23:39:22 -0300

    commit
    commit

---
+++ app/src/main/java/nexus/telepresenca/ListaDispositivos.java
@@ -0,0 +1,64 @@
+package nexus.telepresenca;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Set;
+
+/**
+ * Created by Rocketz on 30/08/2016.
+ */
+public class ListaDispositivos extends ListActivity {
+
+    private BluetoothAdapter bluetoothAdapter = null;
+
+
+   public static String ENDERECO_MAC = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> ArrayBluetooth = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Set<BluetoothDevice> dispostivosParedados = bluetoothAdapter.getBondedDevices();
+
+        if(dispostivosParedados.size() > 0){
+            for(BluetoothDevice dispositivos : dispostivosParedados){
+                String nomeBt = dispositivos.getName();
+                String MacBt = dispositivos.getAddress();
+                ArrayBluetooth.add(nomeBt +  " \n " + MacBt);
+            }
+            setListAdapter(ArrayBluetooth);
+
+        }
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String informacaoGeral = ((TextView ) v).getText().toString();
+
+       // Toast.makeText(getApplicationContext(),"Info: " + informacaoGeral, Toast.LENGTH_LONG).show();
+
+        String enderecoMac = informacaoGeral.substring(informacaoGeral.length() - 17);
+
+        Intent retornaMac = new Intent();
+        retornaMac.putExtra(ENDERECO_MAC, enderecoMac);
+        setResult(RESULT_OK,retornaMac);
+        finish();
+
+
+    }
+}
---
+++ app/src/main/java/nexus/telepresenca/MainActivity.java
@@ -0,0 +1,225 @@
+package nexus.telepresenca;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter bluetoothAdapter = null;
+    BluetoothDevice bluetoothDevice = null;
+    BluetoothSocket bluetoothSocket = null;
+
+    private static final int SOLICITA_ATIVACAO = 1;
+    private static final int SOLICITA_CONEXAO = 2;
+
+    private Button btnConexao;
+    private Button btnFrente;
+    private Button btnVoltar;
+    private Button btnEsquerda;
+    private Button btnDireita;
+    private Button btnStop;
+    private ConnectedThread mConnectedThread;
+    private boolean conexao = false;
+    private static String MAC = null;
+    private UUID  uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnConexao = (Button) findViewById(R.id.btnConectar);
+        btnFrente = (Button) findViewById(R.id.btnFrente);
+        btnVoltar = (Button) findViewById(R.id.btnVoltar);
+        btnDireita = (Button) findViewById(R.id.btnDireita);
+        btnEsquerda = (Button) findViewById(R.id.btnEsquerda);
+        btnStop = (Button) findViewById(R.id.btnStop);
+
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null){
+            Toast.makeText(getApplicationContext(),"Seu dispositivo não possui bluetooth.", Toast.LENGTH_LONG).show();
+        }
+        else if(!bluetoothAdapter.isEnabled() ){
+            Intent  intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, SOLICITA_ATIVACAO);
+
+        }
+
+        btnConexao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(conexao){
+                    //desconectar
+                    try{
+                        conexao = false;
+                        bluetoothSocket.close();
+                        Toast.makeText(getApplicationContext(),"Bleutooth desconectado.", Toast.LENGTH_LONG).show();
+                        btnConexao.setText("Conectar");
+                        mConnectedThread.currentThread().stop();
+                    }
+                    catch (IOException error){
+                        Toast.makeText(getApplicationContext(),"Erro ao desconetar : " + error, Toast.LENGTH_LONG).show();
+                    }
+
+                }
+                else {
+                    //conectar
+
+                    Intent abreLista = new Intent(MainActivity.this,ListaDispositivos.class);
+                    startActivityForResult(abreLista,SOLICITA_CONEXAO);
+                }
+
+            }
+        });
+
+        btnFrente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConnectedThread.write("F");
+            }
+        });
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConnectedThread.write("B");
+            }
+        });
+
+        btnDireita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConnectedThread.write("R");
+            }
+        });
+
+        btnEsquerda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConnectedThread.write("L");
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConnectedThread.write("S");
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case SOLICITA_ATIVACAO:
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(getApplicationContext(),"O bluetooth foi ativado.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"O bluetooth não foi ativado.", Toast.LENGTH_LONG).show();
+                    //finish();
+                }
+                break;
+            case SOLICITA_CONEXAO:
+                if(resultCode == Activity.RESULT_OK){
+
+                    MAC = data.getExtras().getString(ListaDispositivos.ENDERECO_MAC);
+
+                    bluetoothDevice =  bluetoothAdapter.getRemoteDevice(MAC);
+
+                    try{
+                        bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+
+                        bluetoothSocket.connect();
+                        Toast.makeText(getApplicationContext(),"Conectado com MAC: " + MAC, Toast.LENGTH_LONG).show();
+                        conexao = true;
+                        btnConexao.setText("Desconectar");
+                        mConnectedThread = new ConnectedThread(bluetoothSocket);
+                        mConnectedThread.start();
+                    }
+                    catch (IOException error){
+                        conexao = false;
+                        Toast.makeText(getApplicationContext(),"Erro ao conectar : " + error, Toast.LENGTH_LONG).show();
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Falha ao obter o MAC.", Toast.LENGTH_LONG).show();
+                    //finish();
+                }
+                break;
+        }
+
+
+
+    }
+
+    //create new class for connect thread
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        //creation of the connect thread
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                //Create I/O streams for connection
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+//            // Keep looping to listen for received messages
+//            while (true) {
+//                try {
+//                    bytes = mmInStream.read(buffer);        	//read bytes from input buffer
+//                    String readMessage = new String(buffer, 0, bytes);
+//                    // Send the obtained bytes to the UI Activity via handler
+//                   // bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+//                } catch (IOException e) {
+//                    break;
+//                }
+//            }
+        }
+        //write method
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+            } catch (IOException e) {
+                //if you cannot write, close the application
+                Toast.makeText(getBaseContext(), "Connection Failure", Toast.LENGTH_LONG).show();
+                finish();
+
+            }
+        }
+    }
+}
