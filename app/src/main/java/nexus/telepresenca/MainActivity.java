2f80056fc758345d6a4914318e9a7632de699b88
rodolfoizidoro <rodolfoizidoo@gmail.com>
2016-09-16 00:46:44 -0300

    correcoes video
    video

--- app/src/main/java/nexus/telepresenca/MainActivity.java
+++ app/src/main/java/nexus/telepresenca/MainActivity.java
@@ -2,19 +2,28 @@ package nexus.telepresenca;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.sqlite.SQLiteException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -24,18 +33,31 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.opentok.android.BaseVideoRenderer;
+import com.opentok.android.OpentokError;
+import com.opentok.android.Publisher;
+import com.opentok.android.PublisherKit;
+import com.opentok.android.Session;
+import com.opentok.android.Stream;
+import com.opentok.android.Subscriber;
+import com.opentok.android.SubscriberKit;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
 
 import nexus.telepresenca.Util.PermissionUtils;
 import nexus.telepresenca.Video.HelloWorldActivity;
+import nexus.telepresenca.Video.config.OpenTokConfig;
+import nexus.telepresenca.Video.services.ClearNotificationService;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements
+        Session.SessionListener, Publisher.PublisherListener,
+        Subscriber.VideoListener  {
 
     BluetoothAdapter bluetoothAdapter = null;
     BluetoothDevice bluetoothDevice = null;
@@ -55,12 +77,34 @@ public class MainActivity extends AppCompatActivity  {
     private boolean conexao = false;
     private static String MAC = null;
     private UUID  uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-    private SeekBar seekBar;
+  //  private SeekBar seekBar;
     private String URL = "http://www.meucontatoapp.com/nexus/api/select.php";
     ServiceStatus mService;
     boolean mBound = false;
     private PermissionUtils perm;
+    private static final String LOGTAG = "demo-hello-world";
+    private Session mSession;
+    private Publisher mPublisher;
+    private Subscriber mSubscriber;
+    private ArrayList<Stream> mStreams;
+    private Handler mHandler = new Handler();
 
+    private RelativeLayout mPublisherViewContainer;
+    private RelativeLayout mSubscriberViewContainer;
+    private SurveyDBAsyncTask surveyDBAsyncTask;
+
+    // Spinning wheel for loading subscriber view
+    private ProgressBar mLoadingSub;
+
+    private boolean resumeHasRun = false;
+
+    private boolean mIsBound = false;
+    private NotificationCompat.Builder mNotifyBuilder;
+    private NotificationManager mNotificationManager;
+    private ServiceConnection mConnection;
+    private Button btnCamera;
+    private Timer timer;
+    private Button btnFinalizarVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,8 +117,9 @@ public class MainActivity extends AppCompatActivity  {
         btnDireita = (Button) findViewById(R.id.btnDireita);
         btnEsquerda = (Button) findViewById(R.id.btnEsquerda);
         btnStop = (Button) findViewById(R.id.btnStop);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
         btnServidor = (Button) findViewById(R.id.btnServidor);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        btnFinalizarVideo = (Button) findViewById(R.id.btnFinalizarVideo);
 
         String[] permissoes = new String[]{
                 Manifest.permission.RECORD_AUDIO,
@@ -86,6 +131,16 @@ public class MainActivity extends AppCompatActivity  {
         perm = new PermissionUtils();
         perm.validate(this,0,permissoes);
 
+
+        mPublisherViewContainer = (RelativeLayout) findViewById(R.id.publisherview);
+        mSubscriberViewContainer = (RelativeLayout) findViewById(R.id.subscriberview);
+        mLoadingSub = (ProgressBar) findViewById(R.id.loadingSpinner);
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mStreams = new ArrayList<Stream>();
+
+      //  sessionConnect();
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -104,10 +159,13 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(getApplicationContext(),"Toast.", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MainActivity.this, HelloWorldActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                        | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, HelloWorldActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+//                        | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+//                surveyDBAsyncTask=   new SurveyDBAsyncTask();
+//                surveyDBAsyncTask.execute("");
+                sessionConnect();
             }
         });
 
@@ -146,6 +204,25 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
 
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSwapCamera();
+            }
+        });
+        btnFinalizarVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                surveyDBAsyncTask.cancel(true);
+                timer.cancel();
+                timer.purge();
+
+                if (mSession != null) {
+                    mSession.disconnect();
+                }
+            }
+        });
+
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -175,6 +252,311 @@ public class MainActivity extends AppCompatActivity  {
         });
 
 
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mSession != null) {
+            mSession.onPause();
+
+            if (mSubscriber != null) {
+                mSubscriberViewContainer.removeView(mSubscriber.getView());
+            }
+        }
+
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(this.getTitle())
+                .setContentText(getResources().getString(R.string.notification))
+                .setSmallIcon(R.mipmap.ic_launcher).setOngoing(true);
+
+        Intent notificationIntent = new Intent(this, HelloWorldActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent intent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        mNotifyBuilder.setContentIntent(intent);
+        if (mConnection == null) {
+            mConnection = new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName className, IBinder binder) {
+                    ((ClearNotificationService.ClearBinder) binder).service.startService(new Intent(MainActivity.this, ClearNotificationService.class));
+                    NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    mNotificationManager.notify(ClearNotificationService.NOTIFICATION_ID, mNotifyBuilder.build());
+                }
+
+                @Override
+                public void onServiceDisconnected(ComponentName className) {
+                    mConnection = null;
+                }
+
+            };
+        }
+
+        if (!mIsBound) {
+            bindService(new Intent(MainActivity.this,
+                            ClearNotificationService.class), mConnection,
+                    Context.BIND_AUTO_CREATE);
+            mIsBound = true;
+            startService(notificationIntent);
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+
+        if (!resumeHasRun) {
+            resumeHasRun = true;
+            return;
+        } else {
+            if (mSession != null) {
+                mSession.onResume();
+            }
+        }
+        mNotificationManager.cancel(ClearNotificationService.NOTIFICATION_ID);
+
+        reloadInterface();
+    }
+
+    public void onSwapCamera() {
+        if (mPublisher != null) {
+            mPublisher.swapCamera();
+        }
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+
+        if (mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+        if (isFinishing()) {
+            mNotificationManager.cancel(ClearNotificationService.NOTIFICATION_ID);
+            if (mSession != null) {
+                mSession.disconnect();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        mNotificationManager.cancel(ClearNotificationService.NOTIFICATION_ID);
+        if (mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+
+        if (mSession != null) {
+            mSession.disconnect();
+        }
+
+        super.onDestroy();
+        finish();
+    }
+    private void sessionConnect() {
+        if (mSession == null) {
+            mSession = new Session(MainActivity.this,
+                    OpenTokConfig.API_KEY, OpenTokConfig.SESSION_ID);
+            mSession.setSessionListener(this);
+            mSession.connect(OpenTokConfig.TOKEN);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mSession != null) {
+            mSession.disconnect();
+        }
+
+        super.onBackPressed();
+    }
+
+    public void reloadInterface() {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mSubscriber != null) {
+                    attachSubscriberView(mSubscriber);
+                }
+            }
+        }, 500);
+    }
+
+    @Override
+    public void onConnected(Session session) {
+        Log.i(LOGTAG, "Connected to the session.");
+        if (mPublisher == null) {
+            mPublisher = new Publisher(MainActivity.this, "publisher");
+            mPublisher.setPublisherListener(this);
+            attachPublisherView(mPublisher);
+            mSession.publish(mPublisher);
+        }
+    }
+
+    @Override
+    public void onDisconnected(Session session) {
+        Log.i(LOGTAG, "Disconnected from the session.");
+        if (mPublisher != null) {
+            mPublisherViewContainer.removeView(mPublisher.getView());
+        }
+
+        if (mSubscriber != null) {
+            mSubscriberViewContainer.removeView(mSubscriber.getView());
+        }
+
+        mPublisher = null;
+        mSubscriber = null;
+        mStreams.clear();
+        mSession = null;
+    }
+
+    private void subscribeToStream(Stream stream) {
+        mSubscriber = new Subscriber(MainActivity.this, stream);
+        mSubscriber.setVideoListener(this);
+        mSession.subscribe(mSubscriber);
+
+        if (mSubscriber.getSubscribeToVideo()) {
+            // start loading spinning
+            mLoadingSub.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void unsubscribeFromStream(Stream stream) {
+        mStreams.remove(stream);
+        if (mSubscriber.getStream().equals(stream)) {
+            mSubscriberViewContainer.removeView(mSubscriber.getView());
+            mSubscriber = null;
+            if (!mStreams.isEmpty()) {
+                subscribeToStream(mStreams.get(0));
+            }
+        }
+    }
+
+    private void attachSubscriberView(Subscriber subscriber) {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                getResources().getDisplayMetrics().widthPixels, getResources()
+                .getDisplayMetrics().heightPixels);
+        mSubscriberViewContainer.removeView(mSubscriber.getView());
+        mSubscriberViewContainer.addView(mSubscriber.getView(), layoutParams);
+        subscriber.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE,
+                BaseVideoRenderer.STYLE_VIDEO_FILL);
+    }
+
+    private void attachPublisherView(Publisher publisher) {
+        mPublisher.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE,
+                BaseVideoRenderer.STYLE_VIDEO_FILL);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                320, 240);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,
+                RelativeLayout.TRUE);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,
+                RelativeLayout.TRUE);
+        layoutParams.bottomMargin = dpToPx(8);
+        layoutParams.rightMargin = dpToPx(8);
+        mPublisherViewContainer.addView(mPublisher.getView(), layoutParams);
+    }
+
+    @Override
+    public void onError(Session session, OpentokError exception) {
+        Log.i(LOGTAG, "Session exception: " + exception.getMessage());
+    }
+
+    @Override
+    public void onStreamReceived(Session session, Stream stream) {
+
+        if (!OpenTokConfig.SUBSCRIBE_TO_SELF) {
+            mStreams.add(stream);
+            if (mSubscriber == null) {
+                subscribeToStream(stream);
+            }
+        }
+    }
+
+    @Override
+    public void onStreamDropped(Session session, Stream stream) {
+        if (!OpenTokConfig.SUBSCRIBE_TO_SELF) {
+            if (mSubscriber != null) {
+                unsubscribeFromStream(stream);
+            }
+        }
+    }
+
+    @Override
+    public void onStreamCreated(PublisherKit publisher, Stream stream) {
+        if (OpenTokConfig.SUBSCRIBE_TO_SELF) {
+            mStreams.add(stream);
+            if (mSubscriber == null) {
+                subscribeToStream(stream);
+            }
+        }
+    }
+
+    @Override
+    public void onStreamDestroyed(PublisherKit publisher, Stream stream) {
+        if ((OpenTokConfig.SUBSCRIBE_TO_SELF && mSubscriber != null)) {
+            unsubscribeFromStream(stream);
+        }
+    }
+
+    @Override
+    public void onError(PublisherKit publisher, OpentokError exception) {
+        Log.i(LOGTAG, "Publisher exception: " + exception.getMessage());
+    }
+
+    @Override
+    public void onVideoDataReceived(SubscriberKit subscriber) {
+        Log.i(LOGTAG, "First frame received");
+
+        // stop loading spinning
+        mLoadingSub.setVisibility(View.GONE);
+        attachSubscriberView(mSubscriber);
+    }
+
+    /**
+     * Converts dp to real pixels, according to the screen density.
+     *
+     * @param dp A number of density-independent pixels.
+     * @return The equivalent number of real pixels.
+     */
+    private int dpToPx(int dp) {
+        double screenDensity = this.getResources().getDisplayMetrics().density;
+        return (int) (screenDensity * (double) dp);
+    }
+
+    @Override
+    public void onVideoDisabled(SubscriberKit subscriber, String reason) {
+        Log.i(LOGTAG,
+                "Video disabled:" + reason);
+    }
+
+    @Override
+    public void onVideoEnabled(SubscriberKit subscriber, String reason) {
+        Log.i(LOGTAG, "Video enabled:" + reason);
+    }
+
+    @Override
+    public void onVideoDisableWarning(SubscriberKit subscriber) {
+        Log.i(LOGTAG, "Video may be disabled soon due to network quality degradation. Add UI handling here.");
+    }
+
+    @Override
+    public void onVideoDisableWarningLifted(SubscriberKit subscriber) {
+        Log.i(LOGTAG, "Video may no longer be disabled as stream quality improved. Add UI handling here.");
     }
 
     @Override
@@ -209,7 +591,8 @@ public class MainActivity extends AppCompatActivity  {
                         btnConexao.setText("Desconectar");
                         mConnectedThread = new ConnectedThread(bluetoothSocket);
                         mConnectedThread.start();
-                        new SurveyDBAsyncTask().execute("");
+                        surveyDBAsyncTask=   new SurveyDBAsyncTask();
+                        surveyDBAsyncTask.execute("");
                     //    Intent intent = new Intent(this, ServiceStatus.class);
                       //  startService(intent);
                     }
@@ -225,25 +608,7 @@ public class MainActivity extends AppCompatActivity  {
                 break;
         }
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                String velocidade = String.valueOf(progress);
-
-                Toast.makeText(getApplicationContext(),"Velocidade: " + velocidade,Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
 
 
     }
@@ -336,6 +701,7 @@ public class MainActivity extends AppCompatActivity  {
                             @Override
                             public void onResponse(String response) {
                                     mConnectedThread.write(response);
+                                  // Log.i("Comandos",response);
                                 // Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
                                 requestQueue.stop();
 
@@ -359,12 +725,13 @@ public class MainActivity extends AppCompatActivity  {
         protected Long doInBackground(String... params) {
             long id = 0;
             try {
-
-               Timer timer = new Timer();
+                timer = new Timer();
                 timer.schedule(timerTask,2000,1000);
+
 
             }catch (SQLiteException e){
             }
+
             return id;
         }
 
